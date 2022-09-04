c3db7c33c21c52dd66d1892ba0c7321e7613533a
ysj327 <111219166+ysj327@users.noreply.github.com>
2022-09-04 14:35:58 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -79,8 +79,8 @@ export const config = {
    * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
    */
   FESTIVALS: [
-    {"type": "生日", "name": "老婆", "year": "1996", "date": "r09-02"},
-    {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+    {"type": "生日", "name": "老婆", "year": "1998", "date": "05-22"},
+    {"type": "节日", "name": "结婚纪念日", "year": "2021", "date": "09-30"},
  
   ],
 
